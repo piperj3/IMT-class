a4e05702acc8cba528982ed14242b2da9543f43b
piperj3 <piperj3@gmail.com>
2014-09-17 10:57:46 -0500

    Create pushbuttonExample.py

---
+++ pushbuttonExample.py
@@ -0,0 +1,15 @@
+# this example basically tells you when the little button is pressed or not
+# Human Powered Vehicles could use this for a stop light or so...
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    input_state = GPIO.input(18)
+    if input_state == False:
+        print('Button Pressed')
+        time.sleep(0.2)
